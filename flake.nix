464a05fcc3174dad671e67bd44b0624022c3b16b
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2026-09-16 18:36:20 +0100

    feat: expose qmtacp as the default flake package (#7)
    Add packages.qmtacp / apps.qmtacp and set both as default so
    `nix run` and `nix profile add` work. CI now also runs `nix build`.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Rust development shell template";
+  description = "JSON CLI for a running QueryMT ACP WebSocket server";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -7,7 +7,7 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = inputs:
+  outputs = inputs @ {self, ...}:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
 
@@ -18,7 +18,36 @@
         };
 
         rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+
+        qmtacp = pkgs.rustPlatform.buildRustPackage {
+          pname = "qmtacp";
+          version = cargoToml.package.version;
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+          auditable = false;
+          doCheck = false;
+        };
       in {
+        packages = {
+          qmtacp = qmtacp;
+          default = qmtacp;
+        };
+
+        apps = {
+          qmtacp = {
+            type = "app";
+            program = "${self.packages.${system}.qmtacp}/bin/qmtacp";
+          };
+          default = {
+            type = "app";
+            program = "${self.packages.${system}.qmtacp}/bin/qmtacp";
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           packages = [
             rustToolchain
@@ -27,7 +56,7 @@
           ];
 
           shellHook = ''
-            export PS1="(dev:rust) $PS1"
+            export PS1="(dev:qmtacp) $PS1"
           '';
         };
       };
